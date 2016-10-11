5672ff3533358f0c3da2b36f253a0d0baaf0f9cd
郭轩 <guoxuan@guoxuan.local>
2016-10-11 13:30:47 +0800

    添加onboard页面

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,35 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from 'ionic-native';
-
+import { OnboardPage } from '../pages/onboard/onboard';
 import { HomePage } from '../pages/home/home';
-
+import { SecureStorage } from 'ionic-native';
 
 @Component({
   template: `<ion-nav [root]="rootPage"></ion-nav>`
 })
 export class MyApp {
-  rootPage = HomePage;
+  rootPage = null;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
+
+      let secureStorage: SecureStorage = new SecureStorage();
+      secureStorage.get('isFirstLogin').then(() => {
+        this.rootPage = HomePage;
+      }).catch(() => {
+        this.rootPage = OnboardPage;
+        secureStorage.create('babydate').then(() => {
+          secureStorage.set('isFirstLogin', 'true').then().catch(error => {
+            console.log(error);
+          });
+        }).catch(error => {
+          console.log(error);
+        });
+      });
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { OnboardPage } from '../pages/onboard/onboard';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    OnboardPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,8 +16,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    OnboardPage
   ],
   providers: []
 })
-export class AppModule {}
+export class AppModule { }
