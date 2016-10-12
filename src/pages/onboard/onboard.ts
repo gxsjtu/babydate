b281ece267b8bb97ef53bf9de83c4d5d77f0cacc
郭轩 <guoxuan@guoxuan.local>
2016-10-12 10:57:18 +0800

    添加onboard第一个不能前翻 最后一页不能后翻

--- src/pages/onboard/onboard.ts
+++ src/pages/onboard/onboard.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Slides } from 'ionic-angular';
+
 /*
   Generated class for the Onboard page.
 
@@ -11,9 +12,26 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'onboard.html'
 })
 export class OnboardPage {
+  @ViewChild('onboardSlider') slider: Slides;
+  onboardSliderOptions = {
+    pager: true,
+    speed: 20,
+    allowSwipeToPrev: false
+  };
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  onSliderChanged() {
+    this.slider.getSlider().params.allowSwipeToPrev = true;
+    this.slider.getSlider().params.allowSwipeToNext = true;
+    if (this.slider.isBeginning() == true) {
+      this.slider.getSlider().params.allowSwipeToPrev = false;
+    }
+    if (this.slider.isEnd() == true) {
+      this.slider.getSlider().params.allowSwipeToNext = false;
+    }
   }
 
 }
