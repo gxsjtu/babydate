80a182f1766b9469bdfd6b99de1c8e562d12ad7b
郭轩 <guoxuan@guoxuan.local>
2016-10-14 09:44:26 +0800

    Ok

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { GlobalParameters } from '../../providers/global-parameters';
+import { Http } from '@angular/http';
 import * as Swiper from 'swiper';
 
 @Component({
@@ -9,8 +10,12 @@ import * as Swiper from 'swiper';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public gParameters: GlobalParameters) {
-
+  constructor(public navCtrl: NavController, public gParameters: GlobalParameters, public http: Http) {
+    http.get('https://172.20.66.66:3000/hospital/getTops').map(res => res.json()).subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log(error);
+    });
   }
 
   ionViewDidEnter() {
